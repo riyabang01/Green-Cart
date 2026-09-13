64196f533c069f75956ae1c001ed916c9fd1cfcf
Riya Bang <riyabang617@gmail.com>
2026-09-13 14:12:05 +0530

    fix final absolute backend url string

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import axios from 'axios'
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.MODE === 'production' ? '' : 'http://localhost:4000';
+axios.defaults.baseURL = import.meta.env.MODE === 'production' ? 'https://green-cart-theta-eosin.vercel.app' : 'http://localhost:4000';
 
 
 
