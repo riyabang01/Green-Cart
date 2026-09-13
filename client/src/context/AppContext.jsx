204763b92b427e5ee32cd9037348f46510d960af
Riya Bang <riyabang617@gmail.com>
2026-09-13 12:34:48 +0530

    fixed exact production backend URL

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import axios from 'axios'
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.PROD ? 'https://vercel.app' : 'http://localhost:4000';
+axios.defaults.baseURL = import.meta.env.PROD ? 'https://green-cart-theta-eosin.vercel.app' : 'http://localhost:4000';
 
 
 const AppContext = createContext();
