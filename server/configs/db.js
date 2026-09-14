b2fdf11718c8c10ec01e7ee5fe8650e5bf68944e
Riya Bang <riyabang617@gmail.com>
2026-09-14 10:32:59 +0530

    fix: simplify database connection string lookup

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=> {
+const connectDB = async () => {
     try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}`)
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+        await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 }
 
